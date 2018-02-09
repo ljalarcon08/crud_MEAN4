666398df3db401746d5333ce9dc731b2d895dd55
ljalarcon08 <alarcon08@gmail.com>
2018-02-08 22:50:35 -0300

    ninth commit

--- src/index.js
+++ src/index.js
@@ -9,10 +9,8 @@ var app=express();
 var movieModel=require('./models/movie-model');
 var request=require('request');
 
-//var router=require('./routes/index');
 var routerTasks=require('./routes/movies');
 var path=require('path');
-//var server=require('http').Server(app);
 
 app.set('port', process.env.PORT||3000);
 app.set('views',path.join(__dirname,'views'));
@@ -28,24 +26,10 @@ app.use(bodyParser.urlencoded({extended:false}));
 app.use(cookieParser());
 
 
-//app.use('/',router);
 app.use('/api',routerTasks);
-//app.use(router);
+
 app.use(express.static(path.join(__dirname,'dist')));
 
-//app.use(express);
-/*app.use((req,res,next)=>{
-    var err=new Error('Not Found');
-    err.status=404;
-    next(err)
-})
-
-app.use((err,req,res,next)=>{
-    res.locals.message=err.message;
-    res.locals.error=req.app.get('env')==='development'? err:{};
-    res.status(err.status||500);
-    res.render('error');
-})*/
 
 
 
--- src/routes/index.js
+++
@@ -1,8 +0,0 @@
-var router=require('express').Router();
-
-router.get('/',(req, res, next)=>{
-    console.log('HEDAASMDAL');
-    res.render('index.html');
-});
-
-module.exports=router;
